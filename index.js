a1a472e9a01b670f6d9b3e45301940c2c8a6ac1b
lokesh130 <mlokesh.mamta@gmail.com>
2020-04-13 04:35:48 +0530

    path change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('./startup/prod')(app);
 
 //middlewares
 app.use(express.json());
-
+app.use(express.static(front_end));
 
 //database
 const courses = [
@@ -18,7 +18,7 @@ const courses = [
 
 //routes section
 app.get('/',(req,res)=>{
-  res.sendFile('index.html' , { root : __dirname});  
+  res.sendFile('front_end/index.html' , { root : __dirname});
 });
 
 app.get('/api/courses',(req,res)=>{
